28006554cea52a46905fb5af417e4f5735ec1ba9
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2023-10-06 23:51:02 +0300

    add key enter in messageBlock, add button back

--- client/src/components/Message/Chat/Chat.js
+++ client/src/components/Message/Chat/Chat.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from 'react-router-dom';
 import { observer } from "mobx-react-lite";
 import userStore from "../../../store/userStore";
 import messageStore from "../../../store/messageStore";
@@ -9,10 +10,13 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
 import Cookies from "js-cookie";
 import io from "socket.io-client";
+import './Chat.css'
 
 const Chat = observer(() => {
   const [messages, setMessages] = useState(messageStore.messages);
   const [message, setMessage] = useState('');
+  const messagesContainerRef = useRef(null);
+  const navigate = useNavigate();
 
   const id = Cookies.get("chatWith");
   const chatId = Cookies.get("chatId");
@@ -20,8 +24,11 @@ const Chat = observer(() => {
   const data = { autorizedUser, id ,chatId, message };
   const socket = io("http://localhost:5000");
 
-
- 
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   useEffect(() => {
     socket.emit('userId', data);
@@ -31,7 +38,7 @@ const Chat = observer(() => {
     })
     console.log('⚛ --- ⚛ --- ⚛ --- ⚛ ---  >>> ☢ socket.on ☢ messages:', messages)
 
-    socket.on('newMessage', (newMessages) => {
+    socket.on('newMessage', () => {
       socket.emit("messages", data);
     })
 
@@ -46,9 +53,17 @@ const Chat = observer(() => {
     setMessage('');
   };
 
+  const onKeyDown = e =>{
+    if (e.key === 'Enter') sendMessage();
+  }
+
 
   return (
     <div className="main">
+      <div className="button-container_my">
+        <button onClick={() => navigate(-1)} className="custom-button">Back</button>
+      </div>
+      <div className="messages-container" ref={messagesContainerRef}>
       {messages?.map((message) =>
         message.sender.name === userStore.user ? (
           <Seller key={message.id} sellerMessage={message} />
@@ -56,7 +71,7 @@ const Chat = observer(() => {
           <Buyer key={message.id} buyerMessage={message} />
         )
       )}
-
+      </div>
       <div className="ask">
         <h3>Отправить сообщение</h3>
         <InputGroup size="lg">
@@ -67,8 +82,12 @@ const Chat = observer(() => {
             onChange={(e) => {
               setMessage(e.target.value);
             }}
+            onKeyDown={onKeyDown}
           />
-          <InputGroup.Text id="inputGroup-sizing-lg">
+          <InputGroup.Text 
+          id="inputGroup-sizing-lg"
+          onKeyDown={onKeyDown}
+          >
             <Button onClick={sendMessage} variant="grey">
               Отправить
             </Button>
